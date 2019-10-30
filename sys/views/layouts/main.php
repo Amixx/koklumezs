f546882810e2db1b3d1a6a6bf9109137ded82855
janchix <j.polis@datateks.lv>
2019-10-30 13:43:04 +0200

    basic template complete with user login

--- sys/views/layouts/main.php
+++ sys/views/layouts/main.php
@@ -39,8 +39,6 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
