d0a0a0410c14524829b7ae8cd048a88cacd75067
janchix <j.polis@datateks.lv>
2019-11-01 15:44:07 +0200

    lectures got some tabs for convienience

--- sys/views/lectures/_form.php
+++ sys/views/lectures/_form.php
@@ -18,89 +18,110 @@ $ckeditorOptions = ElFinder::ckeditorOptions('elfinder',
 <div class="lectures-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item active">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Lekcija</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="params-tab" data-toggle="tab" href="#params" role="tab" aria-controls="params" aria-selected="false">Parametri</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="hands-tab" data-toggle="tab" href="#hands" role="tab" aria-controls="hands" aria-selected="false">Roku kategorijas</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="evaluations-tab" data-toggle="tab" href="#evaluations" role="tab" aria-controls="evaluations" aria-selected="false">Novērtējumi</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="files-tab" data-toggle="tab" href="#files" role="tab" aria-controls="files" aria-selected="false">Faili</a>
+        </li>        
+    </ul>
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade active in" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <?= $form->field($model, 'title')->textInput() ?>
+            <?= $form->field($model, 'description')->widget(CKEditor::className(),[
+                'editorOptions' => $ckeditorOptions,
+            ]) ?>
+            <?= $form->field($model, 'complexity')->dropDownList(Lectures::getComplexity(), ['prompt' => '']) ?>
+        </div>
+        <div class="tab-pane fade" id="params" role="tabpanel" aria-labelledby="params-tab">
+            <?php if($difficulties){  ?>
+                <h2>Parametri</h2>      
+                <?php      
+                foreach($difficulties as $id => $name){ ?>
+                <div class="form-group field-difficulties-title">
+                    <label class="control-label" for="difficulties-title-<?=$id?>"><?=$name?></label>
+                    <input type="number" min="0" max="10" id="difficulties-title-<?=$id?>" class="form-control" name="difficulties[<?=$id?>]" value="<?=isset($lectureDifficulties[$id]) ? $lectureDifficulties[$id] : ''?>" aria-required="false" aria-invalid="false" />
+                    <div class="help-block"></div>
+                </div>        
+            <?php } ?>       
+            <?php } ?>
+        </div>
+        <div class="tab-pane fade" id="hands" role="tabpanel" aria-labelledby="hands-tab">
+            <?php if($handdifficulties){  ?>
+                <h2>Roku kategorijas</h2>      
+                <?php      
+                if($handdifficulties['left']){ ?>
+                <hr />
+                <h3>Kreisās rokas kategorijas</h3>      
+                <?php foreach($handdifficulties['left'] as $id => $name){ ?>
+                    <div class="form-group field-handdifficulties-title custom-control custom-checkbox mr-sm-2">
+                        <input type="checkbox" class="custom-control-input" id="handdifficulties-title-<?=$id?>" name="handdifficulties[<?=$id?>]" <?=isset($lectureHandDifficulties[$id]) ? 'checked' : ''?> value="1" aria-required="false" aria-invalid="false" />
+                        <label class="custom-control-label" for="handdifficulties-title-<?=$id?>"><?=$name?></label>
+                        <div class="help-block"></div> 
+                    </div>                  
+                <?php }
+                }
+                if($handdifficulties['right']){ ?>
+                    <h3>Labās rokas kategorijas</h3>    
+                    <hr />  
+                    <?php
+                    foreach($handdifficulties['right'] as $id => $name){ ?>
+                    <div class="form-group field-handdifficulties-title custom-control custom-checkbox mr-sm-2">
+                        <input type="checkbox" class="custom-control-input" id="handdifficulties-title-<?=$id?>" name="handdifficulties[<?=$id?>]" <?=isset($lectureHandDifficulties[$id]) ? 'checked' : ''?> value="1" aria-required="false" aria-invalid="false" />
+                        <label class="custom-control-label" for="handdifficulties-title-<?=$id?>"><?=$name?></label>
+                        <div class="help-block"></div> 
+                    </div>         
+                <?php }
+                } ?>       
+            <?php } ?>
+        </div>
+        <div class="tab-pane fade" id="evaluations" role="tabpanel" aria-labelledby="evaluations-tab">
+            <?php if($evaluations){  ?>
+                <h2>Novērtējumi</h2>
+                <hr />      
+                <?php      
+                foreach($evaluations as $id => $evaluation){ ?>
+                    <div class="form-group field-evaluations-title custom-control custom-checkbox mr-sm-2">
+                        <input type="checkbox" class="custom-control-input" id="evaluations-title-<?=$evaluation['id']?>" name="evaluations[<?=$evaluation['id']?>]" <?=isset($lectureEvaluations[$evaluation['id']]) ? 'checked' : ''?> value="1" aria-required="false" aria-invalid="false" />
+                        <label class="custom-control-label" for="evaluations-title-<?=$evaluation['id']?>"><?=$evaluation['title']?> <small>[<?=$evaluation['type']?>]</small></label>
+                        <div class="help-block"></div> 
+                    </div>         
+                <?php } ?>       
+            <?php } ?>
+        </div>
+        <div class="tab-pane fade" id="files" role="tabpanel" aria-labelledby="files-tab">
+            <h2>Pievienotie faili</h2>
+            <p>
+                <a target="_blank" class="btn btn-success" href="/sys/lecturesfiles/create?lecture_id=<?=$model->id?>">Pievienot failu</a>
+            </p>       
+            <?php if($lecturefiles){  ?>        
+                <table class="table table-striped table-bordered">
+                <?php foreach($lecturefiles as $id => $file){ ?>
+                    <tr> 
+                        <td><?=$file['title']?></td>       
+                        <td>
+                            <a target="_blank" href="/sys/lecturesfiles/<?=$file['id']?>" title="Skatīt" aria-label="Skatīt" data-pjax="0"><span class="glyphicon glyphicon-eye-open"></span></a> 
+                            <a target="_blank" href="/sys/lecturesfiles/update/<?=$file['id']?>" title="Rediģēt" aria-label="Rediģēt" data-pjax="0"><span class="glyphicon glyphicon-pencil"></span></a> 
+                            <a href="/sys/lecturesfiles/delete/<?=$file['id']?>" title="Dzēst" aria-label="Dzēst" data-pjax="0" data-confirm="Vai Jūs tiešām vēlaties dzēst šo failu?" data-method="post"><span class="glyphicon glyphicon-trash"></span></a>
+                        </td>
+                    </tr>            
+                <?php } ?>
+                </table>
+            <?php } ?>
 
-    <?= $form->field($model, 'title')->textInput() ?>
-
-    <?= $form->field($model, 'description')->widget(CKEditor::className(),[
-        'editorOptions' => $ckeditorOptions,
-    ]) ?>
-     
+        </div>
+    </div>
     
-
-    <?= $form->field($model, 'complexity')->dropDownList(Lectures::getComplexity(), ['prompt' => '']) ?>
-
-    <?php if($difficulties){  ?>
-        <h2>Parametri</h2>      
-        <?php      
-        foreach($difficulties as $id => $name){ ?>
-        <div class="form-group field-difficulties-title">
-            <label class="control-label" for="difficulties-title-<?=$id?>"><?=$name?></label>
-            <input type="number" min="0" max="10" id="difficulties-title-<?=$id?>" class="form-control" name="difficulties[<?=$id?>]" value="<?=isset($lectureDifficulties[$id]) ? $lectureDifficulties[$id] : ''?>" aria-required="false" aria-invalid="false" />
-            <div class="help-block"></div>
-        </div>        
-       <?php } ?>       
-    <?php } ?>
-
-    <?php if($handdifficulties){  ?>
-        <h2>Roku kategorijas</h2>      
-        <?php      
-        if($handdifficulties['left']){ ?>
-        <hr />
-        <h3>Kreisās rokas kategorijas</h3>      
-        <?php foreach($handdifficulties['left'] as $id => $name){ ?>
-            <div class="form-group field-handdifficulties-title custom-control custom-checkbox mr-sm-2">
-                <input type="checkbox" class="custom-control-input" id="handdifficulties-title-<?=$id?>" name="handdifficulties[<?=$id?>]" <?=isset($lectureHandDifficulties[$id]) ? 'checked' : ''?> value="1" aria-required="false" aria-invalid="false" />
-                <label class="custom-control-label" for="handdifficulties-title-<?=$id?>"><?=$name?></label>
-                <div class="help-block"></div> 
-            </div>                  
-        <?php }
-        }
-        if($handdifficulties['right']){ ?>
-            <h3>Labās rokas kategorijas</h3>    
-            <hr />  
-            <?php
-            foreach($handdifficulties['right'] as $id => $name){ ?>
-            <div class="form-group field-handdifficulties-title custom-control custom-checkbox mr-sm-2">
-                <input type="checkbox" class="custom-control-input" id="handdifficulties-title-<?=$id?>" name="handdifficulties[<?=$id?>]" <?=isset($lectureHandDifficulties[$id]) ? 'checked' : ''?> value="1" aria-required="false" aria-invalid="false" />
-                <label class="custom-control-label" for="handdifficulties-title-<?=$id?>"><?=$name?></label>
-                <div class="help-block"></div> 
-            </div>         
-        <?php }
-        } ?>       
-    <?php } ?>
-
-    <?php if($evaluations){  ?>
-        <h2>Novērtējumi</h2>
-        <hr />      
-        <?php      
-         foreach($evaluations as $id => $evaluation){ ?>
-            <div class="form-group field-evaluations-title custom-control custom-checkbox mr-sm-2">
-                <input type="checkbox" class="custom-control-input" id="evaluations-title-<?=$evaluation['id']?>" name="evaluations[<?=$evaluation['id']?>]" <?=isset($lectureEvaluations[$evaluation['id']]) ? 'checked' : ''?> value="1" aria-required="false" aria-invalid="false" />
-                <label class="custom-control-label" for="evaluations-title-<?=$evaluation['id']?>"><?=$evaluation['title']?> <small>[<?=$evaluation['type']?>]</small></label>
-                <div class="help-block"></div> 
-            </div>         
-        <?php } ?>       
-    <?php } ?>
-    <hr />
-    <h2>Pievienotie faili</h2>
-    <p>
-        <a target="_blank" class="btn btn-success" href="/sys/lecturesfiles/create?lecture_id=<?=$model->id?>">Pievienot failu</a>
-    </p>       
-    <?php if($lecturefiles){  ?>        
-        <table class="table table-striped table-bordered">
-        <?php foreach($lecturefiles as $id => $file){ ?>
-            <tr> 
-                <td><?=$file['title']?></td>       
-                <td>
-                    <a target="_blank" href="/sys/lecturesfiles/<?=$file['id']?>" title="Skatīt" aria-label="Skatīt" data-pjax="0"><span class="glyphicon glyphicon-eye-open"></span></a> 
-                    <a target="_blank" href="/sys/lecturesfiles/update/<?=$file['id']?>" title="Rediģēt" aria-label="Rediģēt" data-pjax="0"><span class="glyphicon glyphicon-pencil"></span></a> 
-                    <a href="/sys/lecturesfiles/delete/<?=$file['id']?>" title="Dzēst" aria-label="Dzēst" data-pjax="0" data-confirm="Vai Jūs tiešām vēlaties dzēst šo failu?" data-method="post"><span class="glyphicon glyphicon-trash"></span></a>
-                </td>
-            </tr>            
-        <?php } ?>
-        </table>
-    <?php } ?>
-
     <div class="form-group">
         <?= Html::submitButton('Saglabāt', ['class' => 'btn btn-success']) ?>
     </div>
