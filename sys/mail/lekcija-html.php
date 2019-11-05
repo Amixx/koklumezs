c392e7a8f923fc41c56457d0869173dcd0ad17e3
janchix <j.polis@datateks.lv>
2019-11-05 11:54:21 +0200

    added automatic mail with link to lecture

--- sys/mail/lekcija-html.php
+++ sys/mail/lekcija-html.php
@@ -2,12 +2,12 @@
 use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $user common\models\User */
-$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
+$link = Yii::$app->urlManager->createAbsoluteUrl(['lekcijas/lekcija', 'id' => $lecture->id]);
 ?>
 <div class="password-reset">
     <p>Sveiki <?= Html::encode($user->first_name) ?>,</p>
 
     <p>Jums ir piešķirta jauna lekcija:</p>
 
-    <p><?= $lecture->title ?></p>
+    <p><?= Html::a(Html::encode($link), $link) ?></p>
 </div>
--- sys/mail/lekcija-text.php
+++ sys/mail/lekcija-text.php
@@ -1,5 +1,11 @@
+<?php
+use yii\helpers\Html;
+/* @var $this yii\web\View */
+/* @var $user common\models\User */
+$link = Yii::$app->urlManager->createAbsoluteUrl(['lekcijas/lekcija', 'id' => $lecture->id]);
+?>
 Sveiki <?= $user->first_name ?>,
 
 Jums ir piešķirta jauna lekcija:
 
-<?= $lecture->title ?>
+<?= $link ?>
