464791ec270b2ce6f97271d48239d33d63bdf350
Imants Liepins <iliepins01@gmail.com>
2020-08-12 13:20:12 +0300

    bugfix - piešķiršanas lapā nestrādāja filtrs pēc valodas

--- sys/web/js/custom.js
+++ sys/web/js/custom.js
@@ -36,9 +36,7 @@ $(document).ready(function() {
 
     setupLectureFilterByDifficulty();
 
-    setupAssignFilterByLanguage();
-
-    setupAssignFilterBySubscriptionType();
+    setupAssignUserListFilters();
 });
 
 function makeNavbarMultilineForStudents(){
@@ -129,140 +127,140 @@ function setupLectureFilterByDifficulty(){
     })
 }
 
-function setupAssignFilterByLanguage(){
-    $("#UserLanguageSelector").on("change", function(){
-        var lang = this.value;
-        modifyViewHrefOnFilter("lang", lang);
-        filterAssignStudentList(lang, $("#UserSubscriptionTypeSelector").val());
-    });    
-}
-
-function setupAssignFilterBySubscriptionType() {
-    // $("#UserSubscriptionTypeSelector").on("change", function () {
-    //     var subType = this.value;
-    //     modifyViewHrefOnFilter("subType", subType);
-    //     filterAssignStudentList($("#UserLanguageSelector").val(), subType);
-    // });
-    
-    
-    $(".subscription-type-selector.type-free").on("change", function () {
-        // var type = this.classList[1].split("-")[1];
-        var subTypes = this.checked ? ["free"] : [];
-        if ($(".subscription-type-selector.type-paid").prop("checked")) subTypes.push("paid");
-        if ($(".subscription-type-selector.type-lead").prop("checked")) subTypes.push("lead");
-        if ($(".subscription-type-selector.type-pausing").prop("checked")) subTypes.push("pausing");
-
-        modifyViewHrefOnFilter("subTypes", subTypes.join(","));
-        filterAssignStudentList($("#UserLanguageSelector").val(), subTypes);
-    });
-    $(".subscription-type-selector.type-paid").on("change", function () {
-        // var type = this.classList[1].split("-")[1];
-        var subTypes = this.checked ? ["paid"] : [];
-        if ($(".subscription-type-selector.type-free").prop("checked")) subTypes.push("free");
-        if ($(".subscription-type-selector.type-lead").prop("checked")) subTypes.push("lead");
-        if ($(".subscription-type-selector.type-pausing").prop("checked")) subTypes.push("pausing");
-
-        modifyViewHrefOnFilter("subTypes", subTypes.join(","));
-        filterAssignStudentList($("#UserLanguageSelector").val(), subTypes);
-    });
-    $(".subscription-type-selector.type-lead").on("change", function () {
-        // var type = this.classList[1].split("-")[1];
-        var subTypes = this.checked ? ["lead"] : [];
-        if ($(".subscription-type-selector.type-paid").prop("checked")) subTypes.push("paid");
-        if ($(".subscription-type-selector.type-free").prop("checked")) subTypes.push("free");
-        if ($(".subscription-type-selector.type-pausing").prop("checked")) subTypes.push("pausing");
-
-        modifyViewHrefOnFilter("subTypes", subTypes.join(","));
-        filterAssignStudentList($("#UserLanguageSelector").val(), subTypes);
-    });
-    $(".subscription-type-selector.type-pausing").on("change", function () {
-        // var type = this.classList[1].split("-")[1];
-        var subTypes = this.checked ? ["pausing"] : [];
-        if ($(".subscription-type-selector.type-paid").prop("checked")) subTypes.push("paid");
-        if ($(".subscription-type-selector.type-free").prop("checked")) subTypes.push("free");
-        if ($(".subscription-type-selector.type-lead").prop("checked")) subTypes.push("lead");
-
-        modifyViewHrefOnFilter("subTypes", subTypes.join(","));
-        filterAssignStudentList($("#UserLanguageSelector").val(), subTypes);
-    });
-
-    $(".subscription-type-selector.type-free").trigger("click");
-    $(".subscription-type-selector.type-paid").trigger("click");
-    $(".subscription-type-selector.type-lead").trigger("click");
-}
-
-function filterAssignStudentList(lang, subTypes){
-    $("#AssignTable tr").each(function () {
-        this.style.display = "";
-    });
-
-    var $langElems = $("td.user-language");
-    $langElems.each(function () {
-        var $subTypeElem = $(this).next()[0];
-        var $userStatusElem = $(this).next().next()[0];
-        var langText = this.innerText;
-        var subTypeText = $subTypeElem.innerText;
-        var isUserPassive = parseInt($userStatusElem.innerText.trim()) === 11;
-       
-        if (shouldHideRow(lang, langText, subTypes, subTypeText, isUserPassive)) {
-            this.parentElement.style.display = "none";
-        }
-    });
-
-    function shouldHideRow(lang, langText, subTypes, subTypeText, isUserPassive){
-        var hideRow;
-        var showAllSubTypes = subTypes.length === 0 || subTypes.length === 3;
-        if (isUserPassive && subTypes.indexOf("pausing") === -1){
-            hideRow = true;
-        }else if (lang === "all") {
-            if (showAllSubTypes) {
-                hideRow = false;
-            } else {
-                hideRow = subTypes.indexOf(subTypeText) === -1;
-            }
-        } else {
-            if (showAllSubTypes) {
-                hideRow = lang !== langText;
-            } else {
-                hideRow = lang !== langText || subTypes.indexOf(subTypeText) === -1;
-            }
-        }
-        return hideRow;
+function setupAssignUserListFilters(){
+    var SUB_TYPES = {
+        free: "free",
+        paid: "paid",
+        lead: "lead",
+        pausing: "pausing"
     }
-}
 
-function modifyViewHrefOnFilter(name, value){
-    $("#AssignTable tbody tr td a[title='Apskatīt']").each(function () {
-        var hrefParts = this.href.split(/[?&]+/)
-        var newHrefPart = value !== "all" ? name+"=" + value : "";
-        if (hrefParts.length === 1) {
-            this.href += "?" + newHrefPart;
-        } else if (hrefParts.length === 2) {            
-            if (hrefParts[1].indexOf(name) !== -1) {
-                if(newHrefPart){
-                    this.href = hrefParts[0] + "?" + newHrefPart;
-                }else{
-                    this.href = hrefParts[0];
-                }          
-            } else {
-                this.href = this.href + "&" + newHrefPart;
+    function getSubTypeSelector(type) {
+        return ".subscription-type-selector.type-" + type;
+    }    
+
+    var subTypeSelectors = Object.keys(SUB_TYPES).map(function (subType) {
+        return getSubTypeSelector(subType);
+    });
+
+    function getSelectedSubTypes() {
+        var subTypes = [];
+
+        if ($(getSubTypeSelector(SUB_TYPES.free)).prop("checked")) subTypes.push("free");
+        if ($(getSubTypeSelector(SUB_TYPES.paid)).prop("checked")) subTypes.push("paid");
+        if ($(getSubTypeSelector(SUB_TYPES.lead)).prop("checked")) subTypes.push("lead");
+        if ($(getSubTypeSelector(SUB_TYPES.pausing)).prop("checked")) subTypes.push("pausing");
+
+        return subTypes;
+    }
+
+    
+
+    function setupAssignFilterByLanguage() {
+        $("#UserLanguageSelector").on("change", function () {
+            var lang = this.value;
+            var subTypes = getSelectedSubTypes();
+
+            modifyViewHrefOnFilter("lang", lang);
+            filterAssignStudentList($("#UserLanguageSelector").val(), subTypes);
+        });
+    }
+
+    function setupAssignFilterBySubscriptionType() {
+        subTypeSelectors.forEach(function (selector) {
+            $(selector).on("change", function () {
+                var subTypes = getSelectedSubTypes();
+
+                modifyViewHrefOnFilter("subTypes", subTypes.join(","));
+                filterAssignStudentList($("#UserLanguageSelector").val(), subTypes);
+            });
+        })
+
+        $(getSubTypeSelector(SUB_TYPES.free)).trigger("click");
+        $(getSubTypeSelector(SUB_TYPES.paid)).trigger("click");
+        $(getSubTypeSelector(SUB_TYPES.lead)).trigger("click");
+    }
+
+    function filterAssignStudentList(lang, subTypes) {
+        $("#AssignTable tr").each(function () {
+            this.style.display = "";
+        });
+
+        var $langElems = $("td.user-language");
+        $langElems.each(function () {
+            var $subTypeElem = $(this).next()[0];
+            var $userStatusElem = $(this).next().next()[0];
+            var langText = this.innerText;
+            var subTypeText = $subTypeElem.innerText;
+            var isUserPassive = parseInt($userStatusElem.innerText.trim()) === 11;
+
+            if (shouldHideRow(lang, langText, subTypes, subTypeText, isUserPassive)) {
+                this.parentElement.style.display = "none";
             }
-        } else {
-            console.log(hrefParts, newHrefPart);
-            if (hrefParts[1].indexOf(name) !== -1) {
-                if(newHrefPart){
-                    this.href = hrefParts[0] + "?" + newHrefPart + "&" + hrefParts[2];
-                }else{
-                    this.href = hrefParts[0] + "?" + hrefParts[2];
-                }                
-            } else {
-                if (newHrefPart) {
-                    this.href = hrefParts[0] + "?" + newHrefPart + "&" + hrefParts[1];
+        });
+
+        function shouldHideRow(lang, langText, subTypes, subTypeText, isUserPassive) {
+            var hideRow;
+            var showAllSubTypes = subTypes.length === 0 || subTypes.length === 4;
+
+            if (lang === "all") {
+                if (showAllSubTypes) {
+                    hideRow = false;
                 } else {
-                    this.href = hrefParts[0] + "?" + hrefParts[1];
-                }      
-                
+                    hideRow = subTypes.indexOf(subTypeText) === -1;
+                }
+            } else {
+                if (showAllSubTypes) {
+                    hideRow = lang !== langText;
+                } else {
+                    hideRow = lang !== langText || subTypes.indexOf(subTypeText) === -1;
+                }
             }
+
+            if (isUserPassive && subTypes.indexOf("pausing") === -1) {
+                hideRow = true;
+            }
+
+            return hideRow;
         }
-    })
+    }
+
+    //TODO: refaktorēt šo
+    function modifyViewHrefOnFilter(name, value) {
+        $("#AssignTable tbody tr td a[title='Apskatīt']").each(function () {
+            var hrefParts = this.href.split(/[?&]+/)
+            var newHrefPart = value !== "all" ? name + "=" + value : "";
+            if (hrefParts.length === 1) {
+                this.href += "?" + newHrefPart;
+            } else if (hrefParts.length === 2) {
+                if (hrefParts[1].indexOf(name) !== -1) {
+                    if (newHrefPart) {
+                        this.href = hrefParts[0] + "?" + newHrefPart;
+                    } else {
+                        this.href = hrefParts[0];
+                    }
+                } else {
+                    this.href = this.href + "&" + newHrefPart;
+                }
+            } else {
+                if (hrefParts[1].indexOf(name) !== -1) {
+                    if (newHrefPart) {
+                        this.href = hrefParts[0] + "?" + newHrefPart + "&" + hrefParts[2];
+                    } else {
+                        this.href = hrefParts[0] + "?" + hrefParts[2];
+                    }
+                } else {
+                    if (newHrefPart) {
+                        this.href = hrefParts[0] + "?" + newHrefPart + "&" + hrefParts[1];
+                    } else {
+                        this.href = hrefParts[0] + "?" + hrefParts[1];
+                    }
+
+                }
+            }
+        })
+    }
+
+    setupAssignFilterByLanguage();
+    setupAssignFilterBySubscriptionType();
 }
