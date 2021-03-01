6b3333d041246547704836f272c511aa093e8c7d
Imants Liepins <iliepins01@gmail.com>
2021-03-01 22:22:33 +0200

    pagaidām salabotas vēl neuztaisītās sadaļas, salabots arhīvs

--- sys/views/archive/video_thumb.php
+++ sys/views/archive/video_thumb.php
@@ -2,19 +2,23 @@
 $hasFiles = false;
 foreach ($lecturefiles as $id => $file) {
     if (isset($file['file']) and !empty($file['file'])) {
-        $path_info = pathinfo($file['file']);
-        if (in_array(strtolower($path_info['extension']), $videos)) {
+        if(strpos($file['file'], "youtube") !== false){
             $hasFiles = true;
-        }
+        }else{
+            $path_info = pathinfo($file['file']);
+            if (in_array(strtolower($path_info['extension']), $videos)) {
+                $hasFiles = true;
+            }
+        }        
     }
 }
 if ($hasFiles) {
     foreach ($lecturefiles as $id => $file) {
-        if (isset($file['file']) and !empty($file['file'])) {
+        if (isset($file['file']) and !empty($file['file']) && strpos($file['file'], "youtube") === false) {
             $path_info = pathinfo($file['file']);
             if (!isset($path_info['extension']) or !in_array(strtolower($path_info['extension']), $videos)) {
                 continue;
-            }
+            }             
         }
 ?>
     <?= isset($file['thumb']) && $file['thumb'] ? $file['thumb'] : "" ?>
