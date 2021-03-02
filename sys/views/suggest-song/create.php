7d97018d589099b89b63446c20c80323085f37a3
Daniels Dambītis <daniels.dam1001@gmail.com>
2021-03-02 14:43:00 +0200

    izveidots create

---
+++ sys/views/suggest-song/create.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Difficulties */
+
+$this->title = \Yii::t('app', 'Create a FAQ');
+ ['label' => \Yii::t('app', 'FAQs'), 'url' => ['index']];
+
+?>
+<div class="suggestion-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+</div>
