ee7953eb1b183c40ab8f133b2183fa26ec0e5f42
Imants Liepins <iliepins01@gmail.com>
2021-03-02 09:22:19 +0200

    layoutos pielikts gtag priekš analītikas

--- sys/views/layouts/main.php
+++ sys/views/layouts/main.php
@@ -36,6 +36,14 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>">
 
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-133287428-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-133287428-1');
+    </script>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
