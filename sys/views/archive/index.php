f933724e55485838a4133dffa0176f2f4b80890f
Daniels Dambītis <daniels.dam1001@gmail.com>
2021-03-04 17:37:10 +0200

    nodarbībām un arhīvam pielikts background

--- sys/views/archive/index.php
+++ sys/views/archive/index.php
@@ -23,7 +23,7 @@ $this->title = \Yii::t('app', 'Archive');
     </div>
     <?php
     if ($archive) { ?>
-        <div class="row">
+        <div class="row wrap-overlay" style="padding: 16px 2px; border-radius: 16px;">
             <?php foreach ($archive as $lecture) {
                 $lecturefiles = Lecturesfiles::getLectureFiles($lecture->id);
                 $userLecture = UserLectures::getUserLectureByLectureId($lecture->id);
