91695dbf229eb7c5bd3e99b0655a3df93efab1a7
Daniels Dambītis <daniels.dam1001@gmail.com>
2021-04-14 10:55:47 +0300

    Pārbauda, vai lietotājs ir aktīvs lekciju piešķiršanā

--- sys/controllers/LekcijasController.php
+++ sys/controllers/LekcijasController.php
@@ -166,6 +166,7 @@ class LekcijasController extends Controller
 
             $nextLessons = UserLectures::getNextLessons($userId);
             $isNextLesson = UserLectures::getIsNextLesson($userId);
+            $isActive =  Users::findIdentity($userId)->status;
 
             return $this->render('overview', [
                 'models' => $models,
@@ -178,6 +179,7 @@ class LekcijasController extends Controller
                 'nextLessons' => $nextLessons,
                 'isNextLesson' => $isNextLesson,
                 'renderRequestButton' => !$user->wants_more_lessons,
+                'isActive' => $isActive,
             ]);
         }
 
