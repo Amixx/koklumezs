c29a2be48f6c04d134a979334d73d7ef3be9749c
Imants Liepins <iliepins01@gmail.com>
2021-05-23 17:15:44 +0300

    pielikts plyr video atskaņotājs

--- sys/views/layouts/components/head.php
+++ sys/views/layouts/components/head.php
@@ -12,6 +12,7 @@ use yii\helpers\Html;
     <link rel="shortcut icon" href="<?= Yii::$app->request->baseUrl; ?>/favicon.png?v=1" type="image/png" />
     <link rel="apple-touch-icon" href="<?= Yii::$app->request->baseUrl; ?>/favicon.png?v=1" type="image/png" />
     <link rel="manifest" href="<?= Yii::$app->request->baseUrl; ?>/manifest.webmanifest?v=1.0.1">
+    <link rel="stylesheet" href="https://cdn.plyr.io/3.6.8/plyr.css" />
     <?php $this->registerCsrfMetaTags() ?> <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
--- sys/web/js/custom.js
+++ sys/web/js/custom.js
@@ -132,6 +132,8 @@ $(document).ready(function() {
     );
 
     $("#sentinvoices-paid_date").attr("autocomplete", "off");
+
+    setupVideoPlayers();
 });
 
 function addPopoverToElement($selector, html){
@@ -574,4 +576,38 @@ $("[id^=lesson_modal]").on("hidden.bs.modal", pausePlayerOnModalClose);
 function pausePlayerOnModalClose(){
     var $pauseBtn = $(this).find(".vjs-play-control.vjs-playing");
     if($pauseBtn) $pauseBtn.trigger('click');
+}
+
+
+function setupVideoPlayers(){
+    var players = {};
+
+    var options = {
+        controls: [
+            'play-large', // The large play button in the center
+            'restart', // Restart playback
+            'rewind', // Rewind by the seek time (default 10 seconds)
+            'play', // Play/pause playback
+            'fast-forward', // Fast forward by the seek time (default 10 seconds)
+            'progress', // The progress bar and scrubber for playback and buffering
+            'current-time', // The current time of playback
+            'duration', // The full duration of the media
+            'mute', // Toggle mute
+            'volume', // Volume control
+            //'captions', // Toggle captions
+            'settings', // Settings menu
+            'pip', // Picture-in-picture (currently Safari only)
+            'airplay', // Airplay (currently Safari only)
+            //'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+            'fullscreen', // Toggle fullscreen
+        ],
+    }
+
+
+    Array.from(document.querySelectorAll("[data-role='player']")).forEach(function(video) {
+        players[video.id] = new Plyr(video, options);
+        setTimeout(() => {
+            players[video.id].poster = posters[video.id];
+        }, 500);
+    });
 }
